85438769a30f9d81c5900f8053adc7edad6ea744
long_vo <vohoangthanhlong97@gmail.com>
2025-04-09 11:05:21 +0700

    CRUD TESTING BY POSTMAN

---
+++ CRUD-Example/src/test/java/com/jts/crud/CrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jts.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Long-backend-java
-This is a website i created for Practicing my backend skill
